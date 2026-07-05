c70c2f54a265d95cfa5559b8124e3bd4b659a162
ezeze1221 <charlyherox@gmail.com>
2026-07-05 18:26:44 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -21,10 +21,10 @@ export const botConfig = {
     // 3 = Watching
     // 4 = Custom
     // 5 = Competing
-    activities: [
+    activities: [Custom
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: ,
+        name: ,/help
         // Activity type number (0 = Playing).
         type: 0,
       },
